927b12c2adf2df6333a79dd9d4860b84b86a7b83
NC <NC@>
2023-04-26 02:04:33 +0700

    cypress: fix

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,6 @@
 describe('Load website', () => {
   it('passes', () => {
     cy.visit(Cypress.env('TEST_URL'))
-      .then((response) => {
-        expect(response.status).to.eq(200)
-        expect(response.body).to.not.be.null
-      })
   })
 })
 
